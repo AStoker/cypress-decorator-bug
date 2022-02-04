6ac2f7e881492c0a635ab1264707604aa6484a44
Andrew Stoker <AStoker@softdocs.com>
2022-02-04 16:06:20 -0500

    Added support for decorators w ith webpack

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,9 +10,13 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const webpackPreprocessor = require('@cypress/webpack-preprocessor')
+
+
 
 /* eslint-disable */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('file:preprocessor', webpackPreprocessor())
 }
