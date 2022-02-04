258ed703e9fa225a096462c6f3115969ce9c5346
Andrew Stoker <AStoker@softdocs.com>
2022-02-04 16:14:46 -0500

    Undid webpack usage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,7 +10,6 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 
 
 
@@ -18,5 +17,5 @@ const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on('file:preprocessor', webpackPreprocessor())
+
 }
