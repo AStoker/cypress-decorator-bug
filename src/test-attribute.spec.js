1613d8c349d047971583e96d1a41c42471ada864
Andrew Stoker <AStoker@softdocs.com>
2022-02-04 16:06:01 -0500

    Fixed test

--- src/test-attribute.spec.js
+++ src/test-attribute.spec.js
@@ -4,6 +4,6 @@ import { TestAttribute } from './test-attribute';
 describe('Test Attribute', () => {
     it('exists', () => {
         let testAttribute = new TestAttribute();
-        expect(testAttribute).toBeDefined();
+        expect(testAttribute).to.exist;
     });
 });
