c4cdbf37f38bcdb86504e9dbde43c74c8e4f61e2
Andrew Stoker <AStoker@softdocs.com>
2022-02-04 16:57:52 -0500

    Trying to use esbuild

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,11 +11,26 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-
+// import { cypressEsbuildPreprocessor}  from 'cypress-esbuild-preprocessor';
+const {cypressEsbuildPreprocessor} = require('cypress-esbuild-preprocessor');
+// const path = require('path');
+// import babel from 'esbuild-plugin-babel';
 
 /* eslint-disable */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-
+  on(
+    'file:preprocessor',
+    cypressEsbuildPreprocessor({
+        esbuildOptions: {
+            // optional tsconfig for typescript support and path mapping (see https://github.com/evanw/esbuild for all options)
+            // tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
+            // plugins: [
+            //   babel()
+            // ]
+        }
+    }),
+);
 }
+
